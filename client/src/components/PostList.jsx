ded166bb44e2dc7ad5d83e9cddbcf5524380cdd8
gaurav-08-dev <g8111997@gmail.com>
2023-09-26 22:50:47 +0530

    created a useAsync hook to manage api calls

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react"
 import { getPosts } from "../services/posts"
 import { Link } from "react-router-dom"
+import { useAsync } from "../hooks/useAsync"
 
 export function PostList() {
+    const { loading, error, value: posts } = useAsync(getPosts)
 
-    const [posts, setPosts] = useState([])
 
-    useEffect(() => {
-        getPosts().then(setPosts)
-    }, [])
+    if(loading) return  <h1>Loading</h1>
+    if(error) return <h1 className="error-msg">{error}</h1>
+    
     return posts.map(post => {
         return (<h1 key={post.id}>
             <Link to={`/posts/${post.id}`}>{post.title}</Link>
